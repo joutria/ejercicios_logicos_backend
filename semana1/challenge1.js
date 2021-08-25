e7c14cf42768d5f65eabd12dc40cacf1ec795974
joutria <joutria@gmail.com>
2021-08-24 21:23:14 -0500

    Ejercicio semana1

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,7 +1,26 @@
 const reverseString = (str) => {
-    //Implementación
-}
+  //Implementación
+  const error = new Error("Tipo de dato o longitud no admitidos.");
+  function reverse(srt) {
+    const x = str.split("");
+    return x
+      .map((_, i) => {
+        return x[x.length - (i + 1)];
+      })
+      .join("");
+  }
+  if (
+    typeof str === "string" &&
+    !str.match(/[0-9]/) &&
+    str.length > 1 &&
+    str.length <= 15
+  ) {
+    return reverse(str);
+  } else {
+    throw error;
+  }
+};
 
 module.exports = {
-    reverseString
-}
+  reverseString,
+};
