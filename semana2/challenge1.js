4c74050575cf5ffe57f0541eff53c39050f09c33
joutria <joutria@gmail.com>
2021-09-01 19:58:05 -0500

    ejercicios semana 2

--- semana2/challenge1.js
+++ semana2/challenge1.js
@@ -1,5 +1,12 @@
 function twoSums(numbs, target) {
     //Implementación
+    for(let i = 0; i < numbs.length; i++) {
+        for(let j = i; j < numbs.length; j++){
+            if (numbs[i]+numbs[j]==target && i!=j){
+                return [i, j];
+            }
+        }
+    }
 }
 
 module.exports = twoSums;
